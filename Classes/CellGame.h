e629e85daf16e0a9efd0b0a2636c177556bb80a8
Ben Walsham <ben.walsham@meltingwaldo.com>
2009-12-29 19:22:37 +0000

    BugId 820 (Game list view (cells)): Added layout for the game rows.

---
+++ Classes/CellGame.h
@@ -0,0 +1,28 @@
+//
+//  CellGame.h
+//
+//  Created by Ben Walsham on 29/12/2009.
+//  Copyright 2009 MeltingWaldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellGame : UITableViewCell {
+  UILabel *nameTeamOne;
+  UILabel *nameTeamTwo;
+  UILabel *pointsTeamOne;
+  UILabel *pointsTeamTwo;
+  UILabel *symbolResultTeamOne;
+  UILabel *symbolResultTeamTwo;
+  UILabel *dateLastPlayed;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *nameTeamOne;
+@property (nonatomic, retain) IBOutlet UILabel *nameTeamTwo;
+@property (nonatomic, retain) IBOutlet UILabel *pointsTeamOne;
+@property (nonatomic, retain) IBOutlet UILabel *pointsTeamTwo;
+@property (nonatomic, retain) IBOutlet UILabel *symbolResultTeamOne;
+@property (nonatomic, retain) IBOutlet UILabel *symbolResultTeamTwo;
+@property (nonatomic, retain) IBOutlet UILabel *dateLastPlayed;
+
+@end
---
+++ Classes/CellGame.m
@@ -0,0 +1,41 @@
+//
+//  CellGame.m
+//
+//  Created by Ben Walsham on 29/12/2009.
+//  Copyright 2009 MeltingWaldo. All rights reserved.
+//
+
+#import "CellGame.h"
+
+@implementation CellGame
+
+@synthesize nameTeamOne;
+@synthesize nameTeamTwo;
+@synthesize pointsTeamOne;
+@synthesize pointsTeamTwo;
+@synthesize symbolResultTeamOne;
+@synthesize symbolResultTeamTwo;
+@synthesize dateLastPlayed;
+
+
+//
+// UITableViewCell functions
+//
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+  if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+  }
+  return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+  
+  [super setSelected:selected animated:animated];
+  
+  // Configure the view for the selected state
+}
+
+- (void)dealloc {
+  [super dealloc];
+}
+
+@end
